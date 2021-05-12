81133fb57b60eed0c37b6a0c759ba461504f317e
Diogo B S <diogoborges14@users.noreply.github.com>
2021-05-12 02:14:42 -0300

    Cadastro de clientes começou a ser implementado

--- main.c
+++ main.c
@@ -1,11 +1,53 @@
 //Função: gerenciar transações financeira
-//Data: 11/05/2021
+//Data início: 11/05/2021
 #include <stdio.h>
-#include <locale.h>
+#include <locale.h> // Localização, caracteres
+#include <string.h> // Trabalhar com strings
 #define EXIT_SUCCESS 0
+#define databaseFile "database.data"
+
+// Define estrutura para endereços tipo _ADDRESS
+typedef struct endereco{
+    char state[20];
+    char city[20];
+    char bairro[20];
+    char street[40];
+    int number;
+    unsigned int zipcode;
+}_ADDRESS;
+
+// Define estrutura para pressoas do tipo _PERSON
+typedef struct pessoa{
+    unsigned int id;
+    char name[40];
+    int cpf_cnpj;
+    int phoneNumber;
+    _ADDRESS address;
+}_PERSON;
+
+// Função para adicionar nova pessoa no banco de dados
+int addNewClient(_PERSON pessoa){
+    // int pessoas_db_tamanho;
+    // FILE *pessoas_db = fopen(databaseFile, "r+");
+
+    // // Armazena o tamanho em bits do arquivo
+    // fseek(pessoas_db, 0, SEEK_END); // Move o cursor para o final do arquivo
+    // pessoas_db_tamanho = ftell(pessoas_db); // Retorna a posição onde o cursor está
+    
+    // // Verifica se o arquivo foi aberto com sucesso
+    // if(pessoas_db == NULL){
+    //     return -1;
+    // }
+    // else if(pessoas_db_tamanho > 0){
+    //     printf("Cheguei aqui. Agora é só desenvolver o cadastro de pessoas. \n");
+    // }
+    return EXIT_SUCCESS;
+}
 
 int main(){
    	setlocale(LC_ALL,"Portuguese");
+    int codigoErro;
+    _PERSON pessoa;
 	char option;
 
     printf(
@@ -41,10 +83,48 @@ int main(){
                 case 'C':
                 case 'c':
                     printf(
-                        "C – Cadastrar um cliente \n"
                         "\033[H\033[2J\033[3J"
-                        "Em implementação... \n"
+                        "C – Cadastrar um cliente \n\n"
+                        "Digite os dados do cliente. \n"
+                        "Nome: "
                     );
+                    scanf(" %s", pessoa.name);
+                    printf("CPF/CNPJ: ");
+                    scanf("%d", &pessoa.cpf_cnpj);
+                    printf("Telefone: ");
+                    scanf("%d", &pessoa.phoneNumber);
+                    printf(
+                        "Digite o endereço.\n"
+                        "Estado: "
+                    );
+                    scanf(" %s", pessoa.address.state);
+                    printf("Cidade: ");
+                    scanf(" %s", pessoa.address.city);
+                    printf("Bairro: ");
+                    scanf(" %s", pessoa.address.bairro);
+                    printf("Rua: ");
+                    scanf(" %s", pessoa.address.street);
+                    printf("Número: ");
+                    scanf("%d", &pessoa.address.number);
+                    printf("CEP: ");
+                    scanf("%d", &pessoa.address.zipcode);
+
+                    // Cadastrar o cliene "pessoa" e armazena possíveis erros em "codigoErro"
+                    codigoErro = addNewClient(pessoa);
+
+                    // Verifica se ocorreram erros
+                    if(codigoErro == EXIT_SUCCESS){
+                        printf(
+                            "\033[H\033[2J\033[3J"
+                            "Cliente cadastrado com sucesso. \n"
+                        );
+                    }else{
+                        printf(
+                            "\033[H\033[2J\033[3J"
+                            "Ocorreu um erro ao cadastrar o cliente. \n"
+                            "Tente novamente. \n"
+                        );
+                    }
                     break;
                 case 'L':
                 case 'l':
